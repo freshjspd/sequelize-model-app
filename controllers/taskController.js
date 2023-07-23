74e5197795937830360c45d46ef13c8e27f7f047
Viktoriia <freshjs.pd@gmail.com>
2023-07-23 10:43:04 +0300

    Add task model

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,11 +1,30 @@
-module.exports.createTask = (req, res) => {};
+const _ = require('lodash');
+const { Task } = require('./../db/models');
 
-module.exports.getTasks = (req, res) => {};
+module.exports.createTask = async (req, res) => {
+  const { body } = req;
 
-module.exports.getTaskById = (req, res) => {
+  try {
+    const createTask = await Task.create(body);
+
+    if (!createTask) {
+      return res.status(500).send('Server Error');
+    }
+
+    const preparedTask = _.omit(createTask.get(), ['updatedAt']);
+    res.status(201).send(preparedTask);
+  } catch (err) {
+    console.log('err :>> ', err);
+    res.status(500).send('Server Error');
+  }
+};
+
+module.exports.getTasks = async (req, res) => {};
+
+module.exports.getTaskById = async (req, res) => {
   res.status(200).send(req.params.taskId);
 };
 
-module.exports.updateTask = (req, res) => {};
+module.exports.updateTask = async (req, res) => {};
 
-module.exports.deleteTask = (req, res) => {};
+module.exports.deleteTask = async (req, res) => {};
---
+++ db/migrations/20230723065150-create-task.js
@@ -0,0 +1,36 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      is_done: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      deadline: {
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tasks');
+  },
+};
