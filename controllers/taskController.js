56fceaba91acca425ff9b9562c713c254412b3dd
Viktoriia <freshjs.pd@gmail.com>
2023-07-23 11:33:07 +0300

    Add tasks controller

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -14,17 +14,80 @@ module.exports.createTask = async (req, res) => {
     const preparedTask = _.omit(createTask.get(), ['updatedAt']);
     res.status(201).send(preparedTask);
   } catch (err) {
-    console.log('err :>> ', err);
     res.status(500).send('Server Error');
   }
 };
 
-module.exports.getTasks = async (req, res) => {};
+module.exports.getTasks = async (req, res) => {
+  try {
+    const foundTasks = await Task.findAll({
+      raw: true,
+      attributes: { exclude: ['updatedAt'] },
+    });
 
-module.exports.getTaskById = async (req, res) => {
-  res.status(200).send(req.params.taskId);
+    res.status(200).send(foundTasks);
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
 };
 
-module.exports.updateTask = async (req, res) => {};
+module.exports.getTaskById = async (req, res) => {
+  const { taskId } = req.params;
 
-module.exports.deleteTask = async (req, res) => {};
+  try {
+    const foundTask = await Task.findByPk(taskId, {
+      raw: true,
+      attributes: { exclude: ['updatedAt'] },
+    });
+
+    if (!foundTask) {
+      return res.status(404).send('Task Not Found');
+    }
+
+    res.status(200).send(foundTask);
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+};
+
+module.exports.updateTask = async (req, res) => {
+  const {
+    body,
+    params: { taskId },
+  } = req;
+
+  try {
+    const [, [updatedTask]] = await Task.update(body, {
+      where: { id: taskId },
+      raw: true,
+      attributes: { exclude: ['updatedAt'] },
+      returning: true,
+    });
+
+    if (!updatedTask) {
+      return res.status(404).send('Task Not Found');
+    }
+
+    res.status(200).send(updatedTask);
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+};
+
+module.exports.deleteTask = async (req, res) => {
+  const { taskId } = req.params;
+
+  try {
+    const deletedTaskCount = await Task.destroy({
+      where: { id: taskId },
+    });
+
+    if (!deletedTaskCount) {
+      return res.status(404).send('Task Not Found');
+    }
+
+    res.status(204).end();
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+};
